b238c79b95dd06ccfcffeb4b355fb5c7c314a1b2
Adam Tyson <adam.tyson@ucl.ac.uk>
2019-12-10 10:52:58 +0000

    bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ requirements = ["configparser", "natsort", "pyyaml"]
 
 setup(
     name="micrometa",
-    version="0.0.5",
+    version="0.0.6",
     description="Reading of microscopy metadata",
     install_requires=requirements,
     extras_require={"dev": ["black", "pytest-cov", "pytest"]},
